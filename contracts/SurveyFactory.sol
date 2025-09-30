08273b8008dae5637282c38421c81d08ba7a062e
hhu-cnu2024 <patek1412@naver.com>
2025-10-01 00:24:43 +0900

    modify access control default to public

--- contracts/SurveyFactory.sol
+++ contracts/SurveyFactory.sol
@@ -13,8 +13,8 @@ struct SurveySchema {
 event SurveyCreated(address);
 
 contract SurveyFactory {
-  uint256 min_pool_amount;
-  uint256 min_reward_amount;
+  uint256 public min_pool_amount;
+  uint256 public min_reward_amount;
   Survey[] surveys;
 
   constructor(uint256 _min_pool_amount, uint256 _min_reward_amount) {
